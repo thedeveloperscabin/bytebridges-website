0c8e3693718c36dc0266b894bfdc7b08d57ca24e
thedeveloperscabin <thedeveloperscabin@gmail.com>
2026-02-22 23:00:14 +0530

    Mobile bug fixed, changes in index.css and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,9 @@ export default function App() {
       <CursorGlow />
       <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none">
 
-      <div className="absolute top-20 left-10 w-96 h-96 bg-indigo-500/20 rounded-full blur-3xl float-slow"></div>
+      <div className="absolute top-20 left-10 w-72 h-72 md:w-96 md:h-96 bg-indigo-500/20 rounded-full blur-3xl float-slow"></div>
 
-      <div className="absolute bottom-20 right-10 w-96 h-96 bg-purple-500/20 rounded-full blur-3xl float-slow delay-1000"></div>
+      <div className="absolute bottom-20 right-10 w-72 h-72 md:w-96 md:h-96 bg-purple-500/20 rounded-full blur-3xl float-slow delay-1000"></div>
 
     </div>
       <Navbar />
